d453985713e37c7208dc39e50c6f798bf4adb44d
Miquido Ops <mqtc@miquido.com>
2021-11-22 10:09:22 +0000

    chore(deps): bump autoscaling from 0.7.1 to 0.7.2

--- main.tf
+++ main.tf
@@ -227,7 +227,7 @@ module "ecs-service-alarms" {
 }
 
 module "autoscaling" {
-  source    = "git::https://github.com/cloudposse/terraform-aws-ecs-cloudwatch-autoscaling.git?ref=tags/0.7.1"
+  source    = "git::https://github.com/cloudposse/terraform-aws-ecs-cloudwatch-autoscaling.git?ref=tags/0.7.2"
   enabled   = var.autoscaling_enabled
   name      = var.name
   namespace = var.project
