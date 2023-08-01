af76fefd1714081377882db0b17881b3640b0a07
Miquido Ops <mqtc@miquido.com>
2023-08-01 21:33:39 +0000

    chore(deps): bump task::terraform-aws-ecs-alb-service-task
    Bumps [task::terraform-aws-ecs-alb-service-task](https://github.com/cloudposse/terraform-aws-ecs-alb-service-task) from 0.66.4 to 0.71.0.
    - [Release notes](https://github.com/cloudposse/terraform-aws-ecs-alb-service-task/releases)
    - [Commits](https://github.com/cloudposse/terraform-aws-ecs-alb-service-task/compare/0.66.4...0.71.0)

--- main.tf
+++ main.tf
@@ -101,7 +101,7 @@ locals {
 }
 
 module "task" {
-  source = "git::https://github.com/cloudposse/terraform-aws-ecs-alb-service-task?ref=0.66.4"
+  source = "git::https://github.com/cloudposse/terraform-aws-ecs-alb-service-task?ref=0.71.0"
 
   name      = var.name
   namespace = var.project
