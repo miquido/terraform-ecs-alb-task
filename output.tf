b85470053634128d3c700f3a0c3d8f2230265388
Marek Mościchowski <marek.moscichowski@miquido.com>
2024-02-22 12:05:04 +0100

    feat: Add service_arn output

--- output.tf
+++ output.tf
@@ -67,3 +67,7 @@ output "app_mesh_service_dns" {
   description = "Service DNS available inside app mesh"
   value       = local.appmesh_service_dns
 }
+
+output "service_arn" {
+  value = module.task.service_arn
+}
